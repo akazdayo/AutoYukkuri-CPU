70a2ace594d383b570a899197a953273165ea175
akaz <82073147+akazdayo@users.noreply.github.com>
2023-11-17 19:56:51 +0900

    CPU用に調整

--- src/run.py
+++ src/run.py
@@ -80,8 +80,6 @@ class Layout:
             ),
         )
 
-        self.speaker_switch = ft.Switch(label="話者認識", value=True)
-
         # ドロップダウンメニュー
         self.model = ft.Dropdown(
             label="モデル",
@@ -105,7 +103,6 @@ class Layout:
                 model=self.model.value,
                 input_path=self.input_file.value,
                 output_path=self.output_file.value,
-                speaker_switch=self.speaker_switch.value,
                 page=self.page,
             ),
             disabled=True
@@ -188,7 +185,6 @@ def create_app(page: ft.Page):
     page.add(
         layout.model,
         layout.status,
-        layout.speaker_switch,
         ft.Row(
             [
                 layout.input_button,
